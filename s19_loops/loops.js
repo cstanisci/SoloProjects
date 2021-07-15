9f31c088768cc4ffd06f97f2f9fcb5fe2e93a8c0
CJ <cjstan22@gmail.com>
2021-07-15 17:30:34 -0400

    todo

--- s19_loops/loops.js
+++ s19_loops/loops.js
@@ -5,3 +5,18 @@ while (!maximum) {
 }
 
 const targetNum = Math.floor(Math.random()* maximum) + 1;
+
+let guess = parseInt(prompt('Enter your first guess?'));
+
+let attempts = 1;
+while(guess !==targetNum) {
+    if(guess === 'q') break;
+    attempts++;
+    if(parseInt(guess) > targetNum) {
+        guess = prompt('Guess too high.  Try again!');
+    } else {
+        guess = prompt('Guess too low.  Try again!');
+    }
+}
+
+console.log(`You got it! It took you ${attempts} tries.`)
