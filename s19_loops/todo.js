cbab4675126c33c16ecb17926c1ed9e843a991db
CJ <cjstan22@gmail.com>
2021-07-18 12:06:59 -0400

    M

--- s19_loops/todo.js
+++ s19_loops/todo.js
@@ -1,29 +1,45 @@
-const todos = [];
-let direction = prompt('What would you like to do?');
+// const todos = [];
+// let direction = prompt('What would you like to do?');
 
-while (direction !== 'quit') {
-	if (direction === 'list') {
-		console.log('*****************');
-		for (let i = 0; i < todos.length; i++) {
-			console.log(`${i}: ${todos[i]}`);
-		}
-		console.log('*****************');
-	} else if (direction === 'new') {
-		let newTodo = prompt('What would you like to add?');
-		todos.push(newTodo);
-		console.log(`${newTodo} added to list`);
-		let direction = prompt('What would you like to do next?');
-	} else if (direction === 'delete') {
-		const deletedToDo = parseInt(
-			prompt('Which todo would you like to delete?')
-		);
-		if (!Number.isNaN(deletedToDo)) {
-			todos.splice(deletedToDo, 1);
-		} else {
-			console.log('Unknown Index');
-		}
+// while (direction !== 'quit') {
+// 	if (direction === 'list') {
+// 		console.log('*****************');
+// 		for (let i = 0; i < todos.length; i++) {
+// 			console.log(`${i}: ${todos[i]}`);
+// 		}
+// 		console.log('*****************');
+// 	} else if (direction === 'new') {
+// 		let newTodo = prompt('What would you like to add?');
+// 		todos.push(newTodo);
+// 		console.log(`${newTodo} added to list`);
+// 		let direction = prompt('What would you like to do next?');
+// 	} else if (direction === 'delete') {
+// 		const deletedToDo = parseInt(
+// 			prompt('Which todo would you like to delete?')
+// 		);
+// 		if (!Number.isNaN(deletedToDo)) {
+// 			todos.splice(deletedToDo, 1);
+// 		} else {
+// 			console.log('Unknown Index');
+// 		}
+// 	}
+// 	direction = prompt('What would you like to do?');
+// }
+
+// console.log('OK, QUIT THE APP');
+
+// let pastes = 'slice';
+// console.log(pastes.slice(1));
+
+const prices = [1, 8, 75, 10, 0.5, 4, 34, 10];
+
+const minPrice = prices.reduce((min, price) => {
+	if (min < price) {
+		min = min;
+	} else {
+		min = price;
 	}
-	direction = prompt('What would you like to do?');
-}
+	return min;
+});
 
-console.log('OK, QUIT THE APP');
+console.log(minPrice);
