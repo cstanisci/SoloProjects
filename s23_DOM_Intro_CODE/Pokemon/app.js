8b01a0b16493d0393ced17047d96557b5a727c2e
CJ <cjstan22@gmail.com>
2021-07-21 12:59:56 -0400

    M

--- s23_DOM_Intro_CODE/Pokemon/app.js
+++ s23_DOM_Intro_CODE/Pokemon/app.js
@@ -1,19 +1,15 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+let container = document.querySelector('#container');
 
-
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
-
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+for (let i = 1; i < 152; i++) {
+	const div = document.createElement('div');
+	let image = document.createElement('img');
+	let span = document.createElement('span');
+	image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+	image.id = i;
+	span.innerText = `${i}`;
+	div.appendChild(image);
+	div.appendChild(span);
+	container.appendChild(div);
 }
