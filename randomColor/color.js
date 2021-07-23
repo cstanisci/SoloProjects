8079850d6268326cba01cc3f95884a5b82220d17
CJ <cjstan22@gmail.com>
2021-07-23 10:47:58 -0400

    M

---
+++ randomColor/color.js
@@ -0,0 +1,16 @@
+let body = document.querySelector('body');
+let button = document.querySelector('button');
+let h1 = document.querySelector('h1');
+
+const makeRandColor = () => {
+	let r = Math.floor(Math.random() * 255) + 1;
+	let g = Math.floor(Math.random() * 255) + 1;
+	let b = Math.floor(Math.random() * 255) + 1;
+	return `rgb(${r}, ${g}, ${b})`;
+};
+
+button.addEventListener('click', function () {
+	const newColor = makeRandColor();
+	body.style.backgroundColor = newColor;
+	h1.innerText = newColor;
+});
